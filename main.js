69b1b7d9939f631403db1fa75dac9d8bc90c68bd
Sean <sean.cowan1@gmail.com>
2020-07-30 17:09:45 -0400

    updated the site to show the two fullstack capstone projects, removed quiz application as it is not approriate for a professional portfolio

--- main.js
+++ main.js
@@ -15,34 +15,7 @@ function initSite() {
 // Intend to replace with database driven source once that module is completed
 function handleProjects() {
     let projectData = {
-        quizApplication: `<section class="project-item">
-        <h3>Solar System Quiz Application</h3>
-        <figure class="project-figure">
-            <img class="project-pics" src="images/quizpic.png" alt="project image">
-            <figcaption class="project-pics-captions">
-                <p>This was developed for the Bloc/Thinkful Web
-                        Developer
-                        course. Requirements were to create a simple
-                        one-directional quiz to practice the core principles of DOM Traversal and Manipulation
-                        via
-                        jQuery.
-                        Responsive mobile-first design with fully validated code. A full brief on all
-                        requirements
-                        is
-                        available in the GitHub repo.</p>
-                        <p><em>Technologies Used: </em>HTML 5, CSS 3, JavaScript, jQuery</p>
-                        <p><em>Live App: </em><a
-                                href="https://seancowan-dev.github.io/Solar-System-Quiz-App/app-index/index.html" target="_blank">Available
-                                here</a>
-                        </p>
-                        <p><em>GitHub Repo: </em><a
-                                href="https://github.com/seancowan-dev/Solar-System-Quiz-App" target="_blank">Available
-                                here</a>
-                        </p>
-            </figcaption>
-        </figure>
-    </section>`,
-    apiCapstone: `<section class="project-item">
+    apiCapstone: `<section class="project-item first">
     <h3>Findthatmovie</h3>
     <figure class="project-figure">
         <img class="project-pics" src="images/moviepic.png" alt="project image">
@@ -59,27 +32,57 @@ function handleProjects() {
                     </p>
         </figcaption>
     </figure>
-</section>`
-    //     placeHolder: `<section class="project-item">
-    //     <h3>Placeholder Project</h3>
-    //     <figure class="project-figure">
-    //         <img class="project-pics" src="images/indev.png" alt="project image">
-    //         <figcaption class="project-pics-captions">
-    //             <p>Default snippet</p>
-    //                     <p><em>Technologies Used: </em>None required.</p>
-    //                     <p><em>Live App: </em>N/A
-    //                     </p>
-    //                     <p><em>GitHub Repo: </em>N/A
-    //                     </p>
-    //         </figcaption>
-    //     </figure>
-    // </section>`
-    };
-    let projectNames = ['quizApplication', 'apiCapstone'];
+</section>`,
+        fullstackCapstone1: `<section class="project-item">
+        <h3>Findthatmovie React</h3>
+        <figure class="project-figure">
+            <img class="project-pics" src="images/moviereact.png" alt="project image">
+            <figcaption class="project-pics-captions">
+                <p>This is a complete rebuild of the Findthatmovie website from my previous project. This application goes fullstack implementing the site on Node/React based architecture.</p>
+                        <p><em>Technologies Used: </em>HTML 5, CSS 3, JavaScript, React, Node, Express, PostgreSQL</p>
+                        <p><em>Live App:</em> <a
+                        href="https://findthatmovie-client.now.sh/" target="_blank">Available
+                        here</a>
+                        </p>
+                        <p><em>GitHub Repo (Client):</em> <a
+                        href="https://github.com/seancowan-dev/findthatmovie-client" target="_blank">Available
+                        here</a>&nbsp;&nbsp;
+                        <p><em>GitHub Repo (API):</em> <a
+                        href="https://github.com/seancowan-dev/findthatmovie-API" target="_blank">Available
+                        here</a>
+                        </p>
+            </figcaption>
+        </figure>
+    </section>`,
+    fullstackCapstone2: `<section class="project-item">
+    <h3>fSocket</h3>
+    <figure class="project-figure">
+        <img class="project-pics" src="images/fsocket.png" alt="project image">
+        <figcaption class="project-pics-captions">
+            <p>This is a websocket based chat room application which allows users to watch Youtube videos together using the most barebones implementation possible.  Simply paste a URL to a YouTube video into the chat, and select it from the playlist to play the video for all users.</p>
+                    <p><em>Technologies Used: </em>HTML 5, CSS 3, JavaScript, React, Node, Express, PostgreSQL and Socket.io</p>
+                    <p><em>Live App:</em> <a
+                    href="https://fsocket-client-git-project-complete.seancowan-dev.now.sh/" target="_blank">Available
+                    here</a>
+                    </p>
+                    <p><em>GitHub Repo (Client):</em> <a
+                    href="https://github.com/seancowan-dev/fsocket-client" target="_blank">Available
+                    here</a>&nbsp;&nbsp;
+                    <p><em>GitHub Repo (API):</em> <a
+                    href="https://github.com/seancowan-dev/fsocket-API" target="_blank">Available
+                    here</a>
+                    </p>
+        </figcaption>
+    </figure>
+</section>`};
+
+    let projectNames = ['apiCapstone', 'fullstackCapstone1', 'fullstackCapstone2'];
 
     for (let i = 0; i < projectNames.length; i++) {
         $('.project-item-container').append(projectData[projectNames[i]]);
     }
+
+    $('.project-item-container').append(`<div class="project-item-spacer"> </div>`);
 
 }
 
